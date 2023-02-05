f902b75ebb5f3d63f08171b1cd259782973f0189
ja <jeanantoine.maro@gmail.com>
2023-02-05 17:39:54 +0100

    API - db auto init

--- CatMashAPI/src/main/resources/import.sql
+++ CatMashAPI/src/main/resources/import.sql
@@ -1,12 +1,12 @@
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE cats;
 
-INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (5,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg','MTgwODA3MA');
-INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (4,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg','tt');
 INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (3,'2023-01-30 14:45:05','http://25.media.tumblr.com/tumblr_m4bgd9OXmw1qioo2oo1_500.jpg','bmp');
-INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (2,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_lzxok2e2kX1qgjltdo1_1280.jpg','c8a');
 INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (1,'2023-01-30 14:45:05','http://25.media.tumblr.com/tumblr_m33r7lpy361qzi9p6o1_500.jpg','3kj');
+INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (4,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg','tt');
+INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (10,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg','MTgwODA3MA');
 INSERT INTO cats(creation_date,picture,json_id) VALUES ('2023-01-30 14:45:05','http://25.media.tumblr.com/tumblr_m2p6dxhxul1qdvz31o1_500.jpg','9pu');
+INSERT INTO cats(popularity,creation_date,picture,json_id) VALUES (2,'2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_lzxok2e2kX1qgjltdo1_1280.jpg','c8a');
 INSERT INTO cats(creation_date,picture,json_id) VALUES ('2023-01-30 14:45:05','http://24.media.tumblr.com/tumblr_m1ku66jPWV1qze0hyo1_400.jpg','aca');
 INSERT INTO cats(creation_date,picture,json_id) VALUES ('2023-01-30 14:45:05','http://25.media.tumblr.com/tumblr_m4pwa9EXE41r6jd7fo1_500.jpg','ebk');
 INSERT INTO cats(creation_date,picture,json_id) VALUES ('2023-01-30 14:45:05','http://25.media.tumblr.com/tumblr_m6x62cjEz31qk4s2co1_1280.jpg','MTg0MTYyOQ');
