77f02f202f51850be26ae0a231f6f2090b673524
ja <jeanantoine.maro@gmail.com>
2023-02-05 20:53:27 +0100

    FRONT - fix app routes

--- catmash-front/src/components/App.js
+++ catmash-front/src/components/App.js
@@ -2,14 +2,16 @@ import '../styles/App.scss';
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import NotFound from "./NotFound";
-import Home from '../Home';
+import CatList from './cats/CatList';
+import CatDuel from './cats/CatDuel';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<CatDuel />} />
+          <Route path="/cats" element={<CatList />} />
           <Route path="*" element={<NotFound />} />
       </Routes>
 
