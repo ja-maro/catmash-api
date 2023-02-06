3b7e1a822a5554226828c88f22377a81693dfc5a
ja <jeanantoine.maro@gmail.com>
2023-02-06 02:08:52 +0100

    docker local ok

--- catmash-front/src/services/cats.service.js
+++ catmash-front/src/services/cats.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:8090/";
-const API_URL = process.env.REACT_APP_CATMASH_API_URL;
+const API_URL = "http://localhost:8090/";
+// const API_URL = process.env.REACT_APP_CATMASH_API_URL;
 
 const getContestants = async (id) => {
     let response;
